94998bda25f52ae967f7add3e11bfc9595cf12d8
耀風 <adwin82627459@gmail.com>
2019-03-23 03:11:24 +0800

    V0.0.2準備和修復登入問題

--- pixiv_downloader.py
+++ pixiv_downloader.py
@@ -1,6 +1,8 @@
 import requests
 import re
 import http.cookiejar
+import json
+import os
 
 class PixivSpider(object):
 	def __init__(self):
@@ -12,7 +14,7 @@ class PixivSpider(object):
 			# 加载cookie
 			self.session.cookies.load(filename='cookies', ignore_discard=True)
 		except:
-			print('cookies不能加载')
+			print('cookies不能載入')
 
 		self.params ={
 			'lang': 'en',
@@ -65,6 +67,41 @@ class PixivSpider(object):
 			self.login()
 		# 儲存cookies
 		self.session.cookies.save(ignore_discard=True, ignore_expires=True)
+
+	def medium(self, ID):
+		url = 'https://www.pixiv.net/ajax/illust/' + ID
+		html = self.session.get(url)
+		x = json.loads(html.text)
+		link = x['body']['urls']['original']
+		self.download(link)
+
+	def download(self, link):
+		urlsplit = str.split(link,'/')
+		if os.path.isfile('./pixiv/' + urlsplit[-1]):
+			print(link + ' 已下載過了')
+		else:
+			img = requests.get(link,headers={'Referer':'https://www.pixiv.net/'})
+			with open ('./pixiv/' + urlsplit[-1] ,'wb') as file:
+				file.write(img.content)
+				file.close
+			print(link + ' 下載完成')
+	
+	def getjson(self, ID):
+		Url = 'https://www.pixiv.net/ajax/user/' + ID + '/profile/all'
+		illustsjson = json.loads(self.session.get(Url).text)
+		illusts = illustsjson['body']['illusts']
+		for key,values in illusts.items():
+			print(key)
+			self.medium(ID=key)
+	
+	def menu(self):
+		print('1.作者UID\n2.作品UID')
+		mode = str(input('請輸入數字選擇：'))
+		if mode == '1':
+			spider.getjson(ID)
+		elif mode == '2':
+			ID = input('請輸入作品ID：')
+			spider.medium(ID)
 
 if __name__ == "__main__":
 	spider = PixivSpider()
